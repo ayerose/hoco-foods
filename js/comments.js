48bff88fd2111567d2e5b82799c53be7f1dd1731
ayerose <reya-k@outlook.de>
2022-01-30 18:48:37 +0000

    ux

---
+++ js/comments.js
@@ -0,0 +1,23 @@
+var totals = {
+	"comments":[
+		{
+			"name": "Laila Sydney",
+			"stars":"5",
+			"date":"",
+			"contents":"Hello neighbors! I'm offering these delicious burgers, served in a brioche bun, iceberg lettuce vine tomato topped with american burger cheese. "
+		},
+		{
+			"name":"Helena Jane",
+			"stars":"4",
+			"date":"",
+			"contents":"I enjoy the meals, and love the fact that the ingredients are really well sourced."
+		},
+		{
+			"name":"Del.L",
+			"stars":"3",
+			"date":"",
+			"contents":"xxxx, xxxxxxx xxxx xxxxx xxxxxxxx xxxxxxxxx xxxxxx xxxxxxx. xxxx xxxxx xxxx xxxx! "
+		}
+	]
+};
+var ratingarr = [5,4,3];
---
+++ js/read_comments.js
@@ -0,0 +1,12 @@
+function read_(){
+	String.prototype.format = function (kwargs) {
+		return this.replace(/\{(\w+)\}/g, function (k, v) {
+		return kwargs[v]
+		});
+	};	
+	$("div").filter(".type1").remove();
+	$.each(totals["comments"],function(index,info){
+		var total = $('<div data-role="content" style="background-color:#6D49C0;" class="type1 ui-content"><p style="text-align: left;font-size:25px;"><i><b>{0}</b></i></p><p style="text-align: left;font-size:15px;color:yellow;"><i><u>{1}</u> star</i></p></div><div data-role="content"style="background-color:#6D49C0;border-bottom:2px dashed black"class="type1 ui-content"><p style="text-align: center;font-size:17px;"><i>{2}</i></p></div>'.format({"0":info["name"],"1":info["stars"],"2":info["contents"]}))
+		$("#commentcontainer").append(total);
+	})
+};
